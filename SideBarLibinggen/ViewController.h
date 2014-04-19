4cda7583a82e05d55df538b7f2966ee1d0698c94
LiBinggen <cralison@163.com>
2014-04-19 23:04:46 +0800

    Initial Commit

---
+++ SideBarLibinggen/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SideBarLibinggen
+//
+//  Created by LiBinggen on 14-4-19.
+//  Copyright (c) 2014年 Libinggen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ SideBarLibinggen/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SideBarLibinggen
+//
+//  Created by LiBinggen on 14-4-19.
+//  Copyright (c) 2014年 Libinggen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
